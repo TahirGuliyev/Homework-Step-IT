c7f305d2b6b7d39178300723cb71d3931fd712f4
Tahir Guliyev <53049809+TahirGuliyev@users.noreply.github.com>
2021-04-14 12:39:36 +0300

    Task1

---
+++ DotNET Homeworks/14.04.2021/Task1.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace HomeWorks
+{
+    class CountSpaces
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sonu noqte ile biten bir cumle ve ya metn daxil edin.");
+            char ch;
+            int spaces = 0;
+            do
+            {
+                ch = (char)Console.Read();
+                if (ch == ' ')
+                {
+                    spaces++;
+                }
+            } while (ch != '.');
+
+            Console.WriteLine("Daxil etdiyiniz metnde bosluqlarin sayi = " + spaces);
+            Console.Read();
+        }
+    }
+}
