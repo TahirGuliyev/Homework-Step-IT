6d164821642b7ff0b8c450e08dfa324480b77984
Tahir Guliyev <53049809+TahirGuliyev@users.noreply.github.com>
2021-04-16 12:49:08 +0400

    Create Program.cs

---
+++ DotNET Homeworks/16.04.2021/Task-2/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using Cars;
+
+namespace HomeWorks
+{
+    class Homework
+    {
+
+        static void Main(string[] args)
+        {
+        Car[] cars = new Car[5];
+           
+            Console.ReadKey();
+        }
+    }
+}
