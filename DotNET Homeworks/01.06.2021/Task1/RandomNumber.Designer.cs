c005077527c1a7487044ba110fa756146b5d14c7
Tahir Guliyev <53049809+TahirGuliyev@users.noreply.github.com>
2021-06-01 21:40:07 +0400

    Task

---
+++ DotNET Homeworks/01.06.2021/Task1/RandomNumber.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace WindowsFormsApp
+{
+    partial class RandomNumber
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonYoxla = new System.Windows.Forms.Button();
+            this.labelNumber = new System.Windows.Forms.Label();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonYoxla
+            // 
+            this.buttonYoxla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonYoxla.Location = new System.Drawing.Point(135, 151);
+            this.buttonYoxla.Name = "buttonYoxla";
+            this.buttonYoxla.Size = new System.Drawing.Size(156, 63);
+            this.buttonYoxla.TabIndex = 0;
+            this.buttonYoxla.Text = "YOXLA";
+            this.buttonYoxla.UseVisualStyleBackColor = true;
+            this.buttonYoxla.Click += new System.EventHandler(this.buttonYoxla_Click);
+            // 
+            // labelNumber
+            // 
+            this.labelNumber.AutoSize = true;
+            this.labelNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelNumber.Location = new System.Drawing.Point(148, 68);
+            this.labelNumber.Name = "labelNumber";
+            this.labelNumber.Size = new System.Drawing.Size(124, 20);
+            this.labelNumber.TabIndex = 1;
+            this.labelNumber.Text = "Ededi Daxil Edin";
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(135, 104);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(156, 20);
+            this.textBoxNumber.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 295);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.labelNumber);
+            this.Controls.Add(this.buttonYoxla);
+            this.Name = "Form1";
+            this.Text = "Random number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonYoxla;
+        private System.Windows.Forms.Label labelNumber;
+        private System.Windows.Forms.TextBox textBoxNumber;
+    }
+}
+
---
+++ DotNET Homeworks/01.06.2021/Task1/RandomNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class RandomNumber : Form
+    {
+        int number = new Random().Next(1, 2000); 
+        public RandomNumber()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonYoxla_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(textBoxNumber.Text) > number) {
+                MessageBox.Show("Daxil etdiyiniz eded komputerin fikrinde tutdugu ededden boyukdur!");
+            }
+            if (int.Parse(textBoxNumber.Text) == number) { 
+            MessageBox.Show($"Ededi duzgun texmin etdiniz! Eded: {number}");
+            }
+            if (int.Parse(textBoxNumber.Text) < number)
+            {
+                MessageBox.Show("Daxil etdiyiniz eded komputerin fikrinde tutdugu ededden kicikdir!");
+            }
+        }
+    }
+}
